8647e240924582d1b89c16915a75bc12eebac7e5
Luis Meza <luismeza01@gmail.com>
2017-03-10 20:39:16 -0600

    javascript countDownClock

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
 
 //plugins
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
+var plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint');
 
 gulp.task('js-compress', function(){
   return gulp.src("pre-js/*.js")
-  .pipe(uglify())
-  .pipe(gulp.dest('js'));
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('sass', function(){
@@ -19,15 +19,15 @@ gulp.task('sass', function(){
     .pipe(gulp.dest("css"));
 });
 
-gulp.task('sass:watch', function(){
-  gulp.watch('pre-css/*.scss', ['sass']);
-});
-
 gulp.task('jshint', function(){
   gulp.src('pre-js/*.js')
-    .pipe(plumber())
     .pipe(jshint())
     .pipe(jshint.reporter("default"));
 });
 
-gulp.task("default", ["sass:watch", "sass", "jshint", "js-compress"]);
+gulp.task('watch', function(){
+  gulp.watch('pre-css/*.scss', ['sass']);
+  gulp.watch('pre-js/*.js', ['js-compress']);
+});
+
+gulp.task("default", ["watch", "sass", "jshint", "js-compress"]);
